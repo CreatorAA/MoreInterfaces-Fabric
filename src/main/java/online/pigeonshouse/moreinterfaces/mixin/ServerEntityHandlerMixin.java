8083e7d2804ef984afd289d37bfcd50671a5f4ff
Create-ADS <2469197826@qq.com>
2024-09-21 22:36:02 +0800

    修复ServerEntityHandlerMixin适配时的低级错误

--- src/main/java/online/pigeonshouse/moreinterfaces/mixin/ServerEntityHandlerMixin.java
+++ src/main/java/online/pigeonshouse/moreinterfaces/mixin/ServerEntityHandlerMixin.java
@@ -4,15 +4,19 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import online.pigeonshouse.moreinterfaces.eventhandler.EntityLoadWorldEventHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net.minecraft.server.level.ServerLevel$EntityCallbacks")
+@Mixin(ServerLevel.class)
 public class ServerEntityHandlerMixin {
-    @Inject(method = "onTrackingStart(Lnet/minecraft/world/entity/Entity;)V", at = @At("TAIL"))
+    @Shadow
+    boolean tickingEntities;
+
+    @Inject(method = "add", at = @At("TAIL"))
     private void onTrackingStart(Entity entity, CallbackInfo ci) {
-        if (EntityLoadWorldEventHandler.INSTANCE != null) {
+        if (!tickingEntities && EntityLoadWorldEventHandler.INSTANCE != null) {
             ServerLevel level = (ServerLevel) entity.level;
             EntityLoadWorldEventHandler.getInstance().onLoad(entity, level);
         }
