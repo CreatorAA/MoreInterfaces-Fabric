1425d03e9df76756601648b1f25e019a577683ad
Create-ADS <2469197826@qq.com>
2024-09-21 17:13:28 +0800

    删除错误的文件

--- src/main/java/online/pigeonshouse/moreinterfaces/moreinterfaces/MoreInterfaces_Fabric1_16_5.java
+++
@@ -1,4 +0,0 @@
-package online.pigeonshouse.moreinterfaces.moreinterfaces;
-
-public class MoreInterfaces_Fabric1_16_5 {
-}
--- src/main/java/online/pigeonshouse/moreinterfaces/moreinterfaces/client/MoreInterfaces_Fabric1_16_5Client.java
+++
@@ -1,4 +0,0 @@
-package online.pigeonshouse.moreinterfaces.moreinterfaces.client;
-
-public class MoreInterfaces_Fabric1_16_5Client {
-}
