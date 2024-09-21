b232e486b1ba9eb7ec0ba8e05e5c48b27717b1b5
Create-ADS <2469197826@qq.com>
2024-09-21 16:28:09 +0800

    第一次提交，1.20.2-1.0.2

---
+++ src/main/java/online/pigeonshouse/moreinterfaces/mixin/ClipContextMixin.java
@@ -0,0 +1,20 @@
+package online.pigeonshouse.moreinterfaces.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClipContext.class)
+public class ClipContextMixin {
+    @Redirect(method = "<init>(Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/level/ClipContext$Block;Lnet/minecraft/world/level/ClipContext$Fluid;Lnet/minecraft/world/entity/Entity;)V",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/shapes/CollisionContext;of(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/world/phys/shapes/CollisionContext;"))
+    private CollisionContext init(Entity entity) {
+        if (entity == null) {
+            return CollisionContext.empty();
+        }
+        return CollisionContext.of(entity);
+    }
+}
